50788aa4931b244d1387a3988cc5e389969b3ba9
Alexander Nestorov <alexandernst@gmail.com>
2015-06-01 10:35:39 +0200

    WIP #7

--- spec/angular-multi-select.spec.js
+++ spec/angular-multi-select.spec.js
@@ -1,40 +1,50 @@
-describe('Testing directive', function() {
-	var scope, elem, directive, compiled, html;
+describe('Testing directive in single mode', function() {
+	var scope, element, directive, html;
 
 	beforeEach(function (){
 		//load the module
 		module('angular-multi-select');
 
 		//set our view html.
-		html = '<div angular-multi-select input-model="items"></div>';
+		html = '<div angular-multi-select input-model="items" output-model="x" group-property="sub" tick-property="check" item-label="{| name |}" selection-mode="single"></div>';
 
 		inject(function($compile, $rootScope) {
 			//create a scope (you could just use $rootScope, I suppose)
-			scope = $rootScope.$new();
-
-			scope.items = [];
+			scope = $rootScope;
 
 			//get the jqLite or jQuery element
-			elem = angular.element(html);
+			element = angular.element(html);
 
 			//compile the element into a function to process the view.
-			compiled = $compile(elem)(scope);
+			$compile(element)($rootScope);
 
-			//call digest on the scope!
-			scope.$apply();
+			element.scope().items = test_data;
+			element.scope().$digest();
 		});
 	});
 
 	it('Should be able to create the directive.', function() {
-		expect(elem).not.toBeEmpty();
+		expect(element).not.toBeEmpty();
 	});
 
 	it('Should create a button', function() {
-		expect(elem).toContainElement('button[ng-bind-html="buttonLabel"]');
+		expect(element).toContainElement('button[ng-bind-html="buttonLabel"]');
 	});
 
-	it('Should create the items layer', function() {
-		expect(elem).toContainElement('div.checkboxLayer');
+	it('Should create the items layer & container', function() {
+		expect(element).toContainElement('div.checkboxLayer');
+		expect(element).toContainElement('div.checkBoxContainer');
+	});
+
+	it('Should contain 3 main categories', function() {
+		expect($('div.checkboxLayer > div', element)).toHaveLength(2);
+		expect($('div.checkBoxContainer > ul > li', element)).toHaveLength(3);
+		expect($('div.checkBoxContainer > ul > li > div > div', element)).toContainText(test_data[0].name);
+	});
+
+	it("Should handle 'hidden' items correctly", function() {
+		expect($('div.checkBoxContainer > ul > li > ul > li', element)[0]).toHaveLength(1);
+		expect($('div.checkBoxContainer > ul > li > ul > li > ul > li div[ng-bind-html="_createLabel(item)"]', element)[0]).toContainText(test_data[0].sub[0].sub[0].name);
 	});
 
 
