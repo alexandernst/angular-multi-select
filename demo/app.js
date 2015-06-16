4e6f47aecb2d427ad3775ddc40b817e899ae7c31
Alexander Nestorov <alexandernst@gmail.com>
2015-06-16 10:49:00 +0200

    Enable filters and grouping in the demo files

--- demo/app.js
+++ demo/app.js
@@ -4,25 +4,35 @@ app.controller('MainCtrl', function($scope) {
 
 	$scope.modernBrowsers = [
 		{
-			icon: '<img  src="https://cdn1.iconfinder.com/data/icons/google_jfk_icons_by_carlosjj/32/chrome.png" />',
-			name: "Chrome"
+			name: "Group A",
+			sub: [
+				{
+					icon: '<img  src="https://cdn1.iconfinder.com/data/icons/google_jfk_icons_by_carlosjj/32/chrome.png" />',
+					name: "Chrome"
+				},
+				{
+					icon: '<img  src="https://cdn2.iconfinder.com/data/icons/humano2/32x32/apps/firefox-icon.png" />',
+					name: "Firefox"
+				},
+				{
+					icon: '<img  src="https://cdn2.iconfinder.com/data/icons/new_google_product_icons_by_carlosjj-dwke/32/chromium.png" />',
+					name: "Chromium"
+				}
+			]
 		},
 		{
-			icon: '<img  src="https://cdn0.iconfinder.com/data/icons/fatcow/32x32/safari_browser.png" />',
-			name: "Safari"
-		},
-		{
-			icon: '<img  src="https://cdn2.iconfinder.com/data/icons/humano2/32x32/apps/firefox-icon.png" />',
-			name: "Firefox"
-		},
-		{
-			icon: '<img  src="https://cdn2.iconfinder.com/data/icons/new_google_product_icons_by_carlosjj-dwke/32/chromium.png" />',
-			name: "Chromium"
-		},
-		{
-			icon: '<img  src="https://cdn1.iconfinder.com/data/icons/logotypes/32/opera-32.png" />',
-			name: "Opera",
-			checked: true
+			name: "Group B",
+			sub: [
+				{
+					icon: '<img  src="https://cdn0.iconfinder.com/data/icons/fatcow/32x32/safari_browser.png" />',
+					name: "Safari"
+				},
+				{
+					icon: '<img  src="https://cdn1.iconfinder.com/data/icons/logotypes/32/opera-32.png" />',
+					name: "Opera",
+					checked: true
+				}
+			]
 		}
 	];
 
