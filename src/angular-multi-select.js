6fdabe9e6f411c336e6b6b55fc582a3a8b78da9d
Alexander Nestorov <alexandernst@gmail.com>
2015-07-08 16:22:10 +0200

    Add a helper function that deeply compares two objects

--- src/angular-multi-select.js
+++ src/angular-multi-select.js
@@ -209,6 +209,30 @@ angular_multi_select.directive('angularMultiSelect', ['$rootScope', '$sce', '$ti
 				return $scope.baseExtend(dst, [].slice.call(arguments, 1), true);
 			};
 
+			$scope.deepCompare = function(x, y){
+				if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+				// after this just checking type of one would be enough
+				if (x.constructor !== y.constructor) { return false; }
+				// if they are functions, they should exactly refer to same one (because of closures)
+				if (x instanceof Function) { return x === y; }
+				// if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+				if (x instanceof RegExp) { return x === y; }
+				if (x === y || x.valueOf() === y.valueOf()) { return true; }
+				if (Array.isArray(x) && x.length !== y.length) { return false; }
+
+				// if they are dates, they must had equal valueOf
+				if (x instanceof Date) { return false; }
+
+				// if they are strictly equal, they both need to be object at least
+				if (!(x instanceof Object)) { return false; }
+				if (!(y instanceof Object)) { return false; }
+
+				// recursive object equality check
+				var p = Object.keys(x);
+				return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
+					p.every(function (i) { return $scope.deepCompare(x[i], y[i]); });
+			};
+
 			/**
 			 * Helper function to inverse the result of a function called by a filter from a template
 			 * @param f
