cbb9793d03fa5ec5defd5ab95d051e6473a5303e
Alexander Nestorov <alexandernst@gmail.com>
2016-01-27 20:11:25 +0100

    Release 5.5.5

--- src/angular-multi-select.js
+++ src/angular-multi-select.js
@@ -3,7 +3,7 @@
  * Creates a dropdown-like widget with check-able items.
  *
  * Project started on: 23 May 2015
- * Current version: 5.5.4
+ * Current version: 5.5.5
  *
  * Released under the MIT License
  * --------------------------------------------------------------------------------
