ded122142c9798a1cddc146352d0d0a0ee6b8bcb
Alexander Nestorov <alexandernst@gmail.com>
2016-02-15 19:43:46 +0100

    Demo is working fine

--- demo/app.js
+++ demo/app.js
@@ -1,10 +1,33 @@
-var app = angular.module('demo', ['angular-multi-select']);
+var app = angular.module('demo', [
+	'angular-multi-select',
+	'angular-multi-select-data-converter',
+	'angular-multi-select-styles-helper'
+]);
 
-app.controller('MainCtrl', function($scope, dataConverter, stylesHelper) {
-	$scope.get_checked_class = stylesHelper.get_checked_class;
-	$scope.get_open_class = stylesHelper.get_open_class;
-	$scope.get_type_class = stylesHelper.get_type_class;
+app.controller('MainCtrl', [
+	'$scope',
+	'$interval',
+	'angularMultiSelect',
+	'angularMultiSelectDataConverter',
+	'angularMultiSelectStylesHelper',
+	function($scope, $interval, angularMultiSelect, angularMultiSelectDataConverter, angularMultiSelectStylesHelper) {
 
-	var checked_data = dataConverter.check_prerequisites(full_data);
-    $scope.items = dataConverter.to_internal(checked_data);
-});
+	$scope.angularMultiSelectStylesHelper = angularMultiSelectStylesHelper;
+	$scope.angularMultiSelect = angularMultiSelect;
+
+
+	console.time('check_prerequisites');
+	var checked_data = angularMultiSelectDataConverter.check_prerequisites(full_data);
+	console.timeEnd('check_prerequisites');
+
+	console.time('to_internal');
+	var internal_data = angularMultiSelectDataConverter.to_internal(checked_data);
+	console.timeEnd('to_internal');
+
+	angularMultiSelect.insert(internal_data);
+
+	$scope.items = angularMultiSelect.get_visible_tree();
+	angularMultiSelect.on_data_change(function () {
+		$scope.items = angularMultiSelect.get_visible_tree();
+	});
+}]);
