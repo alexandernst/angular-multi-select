64411a10f629be2252cc0652d851d76b4f33bae5
Alexander Nestorov <alexandernst@gmail.com>
2016-02-17 00:19:58 +0100

    WIP demo

--- demo/app.js
+++ demo/app.js
@@ -12,22 +12,30 @@ app.controller('MainCtrl', [
 	'angularMultiSelectStylesHelper',
 	function($scope, $interval, angularMultiSelect, angularMultiSelectDataConverter, angularMultiSelectStylesHelper) {
 
-	$scope.angularMultiSelectStylesHelper = angularMultiSelectStylesHelper;
-	$scope.angularMultiSelect = angularMultiSelect;
+		var ams = new angularMultiSelect();
+		var dc = new angularMultiSelectDataConverter();
 
+		$scope.angularMultiSelect = ams;
+		$scope.angularMultiSelectStylesHelper = angularMultiSelectStylesHelper;
 
-	console.time('check_prerequisites');
-	var checked_data = angularMultiSelectDataConverter.check_prerequisites(full_data);
-	console.timeEnd('check_prerequisites');
+		console.time('check_prerequisites');
+		var checked_data = dc.check_prerequisites(full_data);
+		console.timeEnd('check_prerequisites');
 
-	console.time('to_internal');
-	var internal_data = angularMultiSelectDataConverter.to_internal(checked_data);
-	console.timeEnd('to_internal');
+		console.time('to_internal');
+		var internal_data = dc.to_internal(checked_data);
+		console.timeEnd('to_internal');
 
-	angularMultiSelect.insert(internal_data);
+		ams.insert(internal_data);
 
-	$scope.items = angularMultiSelect.get_visible_tree();
-	angularMultiSelect.on_data_change(function () {
-		$scope.items = angularMultiSelect.get_visible_tree();
-	});
-}]);
+		$scope.items = ams.get_visible_tree();
+
+		ams.on_data_change(function () {
+			$scope.items = ams.get_visible_tree();
+		});
+
+		ams.on_visual_change(function () {
+			$scope.items = ams.get_visible_tree();
+		});
+	}
+]);
