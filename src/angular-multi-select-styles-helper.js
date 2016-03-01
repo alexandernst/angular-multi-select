231e9ed80383d68989c4297fa6ea5a609fa9fddb
Alexander Nestorov <alexandernst@gmail.com>
2016-03-01 11:09:31 +0100

    Move the prettifier outside of AMS

--- src/angular-multi-select-styles-helper.js
+++ src/angular-multi-select-styles-helper.js
@@ -1,5 +1,4 @@
 var angular_multi_select_styles_helper = angular.module('angular-multi-select-styles-helper', [
-	'ngSanitize',
 	'angular-multi-select-constants'
 ]);
 
@@ -79,28 +78,3 @@ angular_multi_select_styles_helper.factory('angularMultiSelectStylesHelper', [
 		return StylesHelper;
 	}
 ]);
-
-angular_multi_select_styles_helper.filter('prettify', function () {
-
-	function syntaxHighlight(json) {
-		if (json === null) return;
-		json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-		return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-			var cls = 'ams-json-number';
-			if (/^"/.test(match)) {
-				if (/:$/.test(match)) {
-					cls = 'ams-json-key';
-				} else {
-					cls = 'ams-json-string';
-				}
-			} else if (/true|false/.test(match)) {
-				cls = 'ams-json-boolean';
-			} else if (/null/.test(match)) {
-				cls = 'ams-json-null';
-			}
-			return '<span class="' + cls + '">' + match + '</span>';
-		});
-	}
-
-	return syntaxHighlight;
-});
