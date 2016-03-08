6f3bba48513212ca9131c8c7c3a1035b39b760aa
Alexander Nestorov <alexandernst@gmail.com>
2016-03-08 20:51:52 +0100

    Enable missing specs

--- specs/engine.js
+++ specs/engine.js
@@ -12,10 +12,10 @@ describe('Testing engine', function() {
 	});
 
 	describe('Testing insert method', function () {
-		it('Should be able to insert data and return it back as-is', inject(function () {
-			var de = new angularMultiSelectEngine();
-			de.insert(to_internal_data_1_after);
-			var tree = de.get_full_tree();
+		it('Should be able to insert data and return it back as-is', function () {
+			var e = new angularMultiSelectEngine();
+			e.insert(to_internal_data_1_after);
+			var tree = e.get_full_tree();
 
 			//TODO: remove after https://github.com/techfort/LokiJS/issues/346
 			for (var i = 0; i < tree.length; i++) {
@@ -24,16 +24,22 @@ describe('Testing engine', function() {
 			}
 
 			expect(to_internal_data_1_after).toEqual(tree);
-		}));
+		});
 
-		xit('Should be able to remove old collection before inserting a new one', inject(function () {
-			var de = new angularMultiSelectEngine();
-			de.insert(to_internal_data_1_after);
-			de.insert(to_internal_data_1_after);
-			var tree = de.get_full_tree();
+		it('Should be able to remove old collection before inserting a new one', function () {
+			var e = new angularMultiSelectEngine();
+			e.insert(to_internal_data_1_after);
+			e.insert(to_internal_data_1_after);
+			var tree = e.get_full_tree();
+
+			//TODO: remove after https://github.com/techfort/LokiJS/issues/346
+			for (var i = 0; i < tree.length; i++) {
+				delete tree[i].meta;
+				delete tree[i].$loki;
+			}
 
 			expect(to_internal_data_1_after).toEqual(tree);
-		}));
+		});
 
 	});
 
