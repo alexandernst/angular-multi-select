1f4221b5048020c15f9157b1eddd416c595a98f6
Alexander Nestorov <alexandernst@gmail.com>
2016-03-11 15:52:21 +0100

    To tab

--- utils/generate_big_dataset.js
+++ utils/generate_big_dataset.js
@@ -3,37 +3,37 @@ DataFixture = require('datafixture.js');
 
 var id = 0;
 function getId() {
-    return id++;
+	return id++;
 }
 
 function randomString(len, an){
-    an = an&&an.toLowerCase();
-    var str="", i=0, min=an=="a"?10:0, max=an=="n"?10:62;
-    for(;i++<len;){
-      var r = Math.random()*(max-min)+min <<0;
-      str += String.fromCharCode(r+=r>9?r<36?55:61:48);
-    }
-    return str;
+	an = an&&an.toLowerCase();
+	var str="", i=0, min=an=="a"?10:0, max=an=="n"?10:62;
+	for(;i++<len;){
+		var r = Math.random()*(max-min)+min <<0;
+		str += String.fromCharCode(r+=r>9?r<36?55:61:48);
+	}
+	return str;
 }
 
 var template = {
-    text: function () { return randomString(10) },
-    value: function () { return getId() },
-    children: {
-        '#': "0...15",
-        text: function () { return randomString(10) },
-        value: function () { return getId() },
-        children: {
-            '#': "0...10",
-            text: function () { return randomString(10) },
-            value: function () { return getId() },
-            children: {
-                '#': "0...5",
-                text: function () { return randomString(10) },
-                value: function () { return getId() }
-            }
-        }
-    }
+	text: function () { return randomString(10) },
+	value: function () { return getId() },
+	children: {
+		'#': "0...15",
+		text: function () { return randomString(10) },
+		value: function () { return getId() },
+		children: {
+			'#': "0...10",
+			text: function () { return randomString(10) },
+			value: function () { return getId() },
+			children: {
+				'#': "0...5",
+				text: function () { return randomString(10) },
+				value: function () { return getId() }
+			}
+		}
+	}
 };
 
 var res = DataFixture.generate(template, 300);
