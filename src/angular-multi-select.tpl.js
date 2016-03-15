8377c3038383b291d20ac3a981e9f2cd23387fd4
Alexander Nestorov <alexandernst@gmail.com>
2016-03-15 02:40:37 +0100

    Namespace more css classes

--- src/angular-multi-select.tpl.js
+++ src/angular-multi-select.tpl.js
@@ -5,26 +5,26 @@ angular_multi_select.run(['$templateCache', function($templateCache) {
 
 		<div class="ams-button" ng-click="open = !open">
 			<div class="ams-button-text" ng-bind-html="amssh.create_dropdown_label(stats)"></div>
-			<div class="caret"></div>
+			<div class="ams-caret"></div>
 		</div>
 
 		<div class="ams-container ng-cloak" ng-show="open">
 
 			<div class="ams-helpers">
 				<div class="selects">
-					<button class="all btn" type="button" ng-click="amse.check_all()" ng-hide="hide_helpers.indexOf('check_all') > -1">Check all</button>
-					<button class="none btn" type="button" ng-click="amse.uncheck_all()" ng-hide="hide_helpers.indexOf('check_none') > -1">Check none</button>
+					<button class="all ams-btn" type="button" ng-click="amse.check_all()" ng-hide="hide_helpers.indexOf('check_all') > -1">Check all</button>
+					<button class="none ams-btn" type="button" ng-click="amse.uncheck_all()" ng-hide="hide_helpers.indexOf('check_none') > -1">Check none</button>
 				</div>
 
 				<div class="resets">
-					<button class="reset btn" type="button" ng-click="reset()" ng-hide="hide_helpers.indexOf('reset') > -1">Reset data</button>
+					<button class="reset ams-btn" type="button" ng-click="reset()" ng-hide="hide_helpers.indexOf('reset') > -1">Reset data</button>
 				</div>
 			</div>
 
 			<div class="ams-search" ng-show="search_field !== null">
 				<input class="ams-search-field" type="text" name="ams-search-field" value="" ng-model="search" autofocus>
 				<div class="ams-spinner" ng-show="search_spinner_visible"></div>
-				<button class="clear btn" type="button" name="clear" ng-click="search = ''"></button>
+				<button class="clear ams-btn" type="button" name="clear" ng-click="search = ''"></button>
 			</div>
 
 			<div class="ams-items">
@@ -34,7 +34,7 @@ angular_multi_select.run(['$templateCache', function($templateCache) {
 				>
 					<!-- Caret -->
 					<div
-						class="caret {{ amssh.get_open_class(item) }}"
+						class="ams-caret {{ amssh.get_open_class(item) }}"
 						ng-click="amse.toggle_open_node(item)"
 					></div>
 
