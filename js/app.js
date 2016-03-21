3513d1a2f59d0326622402d7c82297ee0d0cf84f
Alexander Nestorov <alexandernst@gmail.com>
2016-03-21 14:48:57 +0100

    Add GA

--- js/app.js
+++ js/app.js
@@ -5,6 +5,14 @@ var demo = angular.module('demo', [
 	'angular-multi-select'
 ])
 
+.run([
+	'$rootScope', '$window', '$location', function($rootScope, $window, $location) {
+		$rootScope.$on('$routeChangeSuccess', function() {
+			$window.ga('send', 'pageview', {page: $location.url()});
+		});
+	}
+])
+
 .filter('prettify', function ($sce) {
 	function syntaxHighlight(json) {
 		if (json === null) return;
@@ -119,3 +127,9 @@ var demo = angular.module('demo', [
 		redirectTo: '/main'
 	});
 }]);
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-27952342-7', 'auto');
