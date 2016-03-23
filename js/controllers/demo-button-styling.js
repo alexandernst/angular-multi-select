09a6f3374bc25648877be6ade41bdbd1d0f870df
Alexander Nestorov <alexandernst@gmail.com>
2016-03-23 14:43:06 +0100

    Add docs for button styling

---
+++ js/controllers/demo-button-styling.js
@@ -0,0 +1,15 @@
+demo.controller('demoButtonStyling', ['$scope', function ($scope) {
+
+	$scope.input_data = [
+		{ text: 'A', value: 'a', id: 1},
+		{ text: 'B', value: 'b', id: 2},
+		{ text: 'C', value: 'c', id: 3, checked: true},
+		{ text: 'D', value: 'd', id: 4},
+		{ text: 'E', value: 'e', id: 5},
+		{ text: 'F', value: 'f', id: 6, checked: true}
+	];
+
+	$scope.output_data_1 = [];
+	$scope.output_data_2 = [];
+
+}]);
