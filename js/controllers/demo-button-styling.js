cd319a3f11b7ae56b485b8e53775d0f6b0eca7e4
Alexander Nestorov <alexandernst@gmail.com>
2016-04-07 12:18:53 +0200

    Add more demos

--- js/controllers/demo-button-styling.js
+++ js/controllers/demo-button-styling.js
@@ -9,7 +9,39 @@ demo.controller('demoButtonStyling', ['$scope', function ($scope) {
 		{ text: 'F', value: 'f', id: 6, checked: true}
 	];
 
+	$scope.input_data_2 = [
+		{ text: 'A', value: 'a', id: 1, children: [
+			{ text: 'A 1', value: 'a1', id: 11},
+			{ text: 'A 2', value: 'a2', id: 12},
+			{ text: 'A 3', value: 'a3', id: 13, children: [
+				{ text: 'A 11', value: 'a11', id: 111},
+				{ text: 'A 12', value: 'a12', id: 112},
+				{ text: 'A 13', value: 'a13', id: 113}
+			]},
+		]},
+		{ text: 'B', value: 'b', id: 2, children: [
+			{ text: 'B 1', value: 'b1', id: 21},
+			{ text: 'B 2', value: 'b2', id: 22},
+		]},
+		{ text: 'C', value: 'c', id: 3},
+		{ text: 'D', value: 'd', id: 4, children: [
+			{ text: 'D 1', value: 'd1', id: 41, children: [
+				{ text: 'D 11', value: 'd11', id: 411, children: [
+					{ text: 'D 111', value: 'd111', id: 4111, children: [
+						{ text: 'D 1111', value: 'd1111', id: 41111}
+					]}
+				]}
+			]}
+		]},
+		{ text: 'E', value: 'e', id: 5},
+		{ text: 'F', value: 'f', id: 6, children: [
+			{ text: 'F 1', value: 'f1', id: 61},
+			{ text: 'F 2', value: 'f2', id: 62},
+		]}
+	];
+
 	$scope.output_data_1 = [];
 	$scope.output_data_2 = [];
+	$scope.output_data_3 = [];
 
 }]);
